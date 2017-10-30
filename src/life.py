5aee4be7ed388cb8ca0e355d94deceb0afabbb0e
Alexander Plesovskikh <alexander.plesovskikh@genestack.com>
2017-10-30 10:36:53 +0300

    Add simple docstring

--- src/life.py
+++ src/life.py
@@ -2,6 +2,10 @@ import numpy as np
 
 
 def life(space):
+    """
+    :param space: two-dimensional matrix (non-empty)
+    :return: next tick of game space
+    """
     space = np.array(space)
     if not (len(space.shape) == 2 and space.shape[0] >= 1 and space.shape[1] >= 1):
         raise TypeError("space should be non-empty two-dimensional matrix")
