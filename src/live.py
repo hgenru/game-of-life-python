d0372196ed0f508ac94cae0db6fa7b183ed84375
Alexander Plesovskikh <alexander.plesovskikh@genestack.com>
2017-10-30 12:14:40 +0300

    Implement live function

--- src/live.py
+++ src/live.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.ndimage.filters import generic_filter
 
 
 def live(space):
@@ -9,4 +10,24 @@ def live(space):
     space = np.array(space)
     if not (len(space.shape) == 2 and space.shape[0] >= 1 and space.shape[1] >= 1):
         raise TypeError("space should be non-empty two-dimensional matrix")
-    pass
+    return generic_filter(space, live_filter, size=(3, 3))
+
+
+NEIGHBORS_MASK = np.array([
+    [1, 1, 1],
+    [1, 0, 1],
+    [1, 1, 1]
+], dtype=bool)
+
+
+def live_filter(cell_with_neighbors):
+    cell_with_neighbors = cell_with_neighbors.reshape(NEIGHBORS_MASK.shape)
+    neighbors = cell_with_neighbors[NEIGHBORS_MASK]
+    neighbors_count = neighbors.sum()
+    if neighbors_count == 2:
+        return cell_with_neighbors.item((1, 1))
+    if neighbors_count == 3:
+        return 1
+    if neighbors_count > 3:
+        return 0
+    return 0
--- tests/test_live.py
+++ tests/test_live.py
@@ -1,4 +1,5 @@
-from nose.tools import raises
+import numpy as np
+from nose.tools import raises, ok_
 from src.live import live
 
 
@@ -10,3 +11,81 @@ def test_should_raise_type_error_on_zero_none_shape():
 @raises(TypeError)
 def test_should_raise_type_error_on_zero_zero_shape():
     live([[]])
+
+
+def test_the_dead_should_not_get_up():
+    shape = (3, 3)
+    ok_(
+        np.array_equal(live(np.zeros(shape)), np.zeros(shape)),
+        "Died cells should'nt get up"
+    )
+
+
+def test_alone_cell_should_die():
+    space = np.array([
+        [0, 0, 0],
+        [0, 1, 0],
+        [0, 0, 0]
+    ])
+    ok_(
+        np.array_equal(live(space), np.zeros((3, 3))),
+        "Alone cell should die"
+    )
+
+
+def test_blok_should_be_always_live():
+    space = np.array([
+        [0, 0, 0, 0],
+        [0, 1, 1, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0],
+    ])
+    ok_(
+        np.array_equal(live(space), space),
+        "Block should be always live"
+    )
+
+
+def test_blinker_should_blink():
+    space = np.array([
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0],
+    ])
+    expected = np.array([
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ])
+    ok_(
+        np.array_equal(live(space), expected),
+        "Blinker should blink"
+    )
+
+
+def test_beacon_should_live():
+    space = np.array([
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 0, 0, 0],
+        [0, 1, 1, 0, 0, 0],
+        [0, 0, 0, 1, 1, 0],
+        [0, 0, 0, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0],
+    ])
+    expected = np.array([
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0],
+    ])
+    ok_(
+        np.array_equal(live(space), expected),
+        "Beacon should live"
+    )
+
